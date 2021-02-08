49845f03d6e7328b144c4cdf637f7d03da26dbb7
Dead Nate <nathan.yukio.stephens@gmail.com>
2021-02-08 11:29:35 -0800

    9.2 reference

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const profileDataArgs = process.argv.slice(2);
 
 const [name, github] = profileDataArgs;
 
+const pageHTML = generatePage(name, github);
 
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
+fs.writeFile('./index.html', pageHTML, err => {
     if (err) throw err;
     console.log('Portfolio complete! Check out index.html to see the output!');
 });
